9ced103fa805e2e281762d56211dfe9879b2c193
Madhav <vmadhav1805@gmail.com>
2025-09-25 17:15:51 +0530

    new

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState, useRef } from "react";
 import useLocalstorage from "../hooks/useLocalstorage";
 
 const Home = () => {
   const [count, setCount] = useLocalstorage("count", 0);
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  // function useDebounce(fn: () => void, delay: number, deps: any[]) {
+  //     const timerId = useRef<ReturnType<typeof setTimeout>>(null);
+
+  //     useEffect(() => {
+  //         if (timerId.current) clearTimeout(timerId.current);
+  //         timerId.current = setTimeout(() => {
+  //             fn();
+  //         }, delay);
+
+  //         return () => {
+  //             if (timerId.current) clearTimeout(timerId.current);
+  //         };
+  //     }, deps);
+  // }
+
+  const fetchAPI = () => {
+    console.log("I'm fetching API...");
+  };
+
+  // useDebounce(fetchAPI, 500, [value]);
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      fetchAPI();
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [value]);
+
   return (
     <div>
       {count}
@@ -11,6 +49,7 @@ const Home = () => {
       <button onClick={() => setCount((prev: number) => prev - 1)}>
         Decrease
       </button>
+      <input value={value} onChange={handleChange} />
     </div>
   );
 };
